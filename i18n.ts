3eedbf90172dca11176f51f67435f301b984cc02
Baran <Baran@LAPTOP-RLQ1AEIK>
2025-05-06 22:22:11 +0200

    i18n changes

--- i18n.ts
+++ i18n.ts
@@ -1,10 +1,21 @@
+export const locales = ['en', 'de'];
+export const defaultLocale = 'en';
+export type Locale = (typeof locales)[number];
+
 import {getRequestConfig} from 'next-intl/server';
 
 export default getRequestConfig(async ({locale}) => {
-  const safeLocale = locale || 'en';
+  const safeLocale = locale || defaultLocale;
   
   return {
     locale: safeLocale,
     messages: (await import(`./messages/${safeLocale}.json`)).default
   };
 });
+
+export function getStaticPaths() {
+  return {
+    paths: locales.map((locale) => ({ params: { locale } })),
+    fallback: false
+  };
+}
